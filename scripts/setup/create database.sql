8e54ef2302d544c094f78b3f343db3dbce824ab6
Timothy Addison <tim@tjaddison.com>
2016-05-22 15:36:26 +0000

    Add simple recovery, update drive paths

--- scripts/setup/create database.sql
+++ scripts/setup/create database.sql
@@ -2,17 +2,19 @@
 	- Key thing is to ensure files are large enough for us to not to autogrow during testing
  */
 create database TAQBenchmarks on primary 
-( name = N'TAQBenchmarks', filename = N'B:\SQL\TAQBenchmarks.mdf' , size = 1048576kb , filegrowth = 256000kb )
+( name = N'TAQBenchmarks', filename = N'D:\SQL\TAQBenchmarks.mdf' , size = 1048576kb , filegrowth = 256000kb )
     log on 
-( name = N'TAQBenchmarks_log', filename = N'B:\SQL\TAQBenchmarks_log.ldf' , size = 524288kb , filegrowth = 256000kb );
+( name = N'TAQBenchmarks_log', filename = N'D:\SQL\TAQBenchmarks_log.ldf' , size = 524288kb , filegrowth = 256000kb );
 go
-alter database TAQBenchmarks set compatibility_level = 120;
+alter database TAQBenchmarks set compatibility_level = 130;
 go
 alter database TAQBenchmarks set enable_broker; 
 go
 alter database TAQBenchmarks set read_committed_snapshot on; 
 go
 alter database TAQBenchmarks set delayed_durability = allowed;
+go
+alter database TAQBenchmarks set recovery simple;
 go
 alter authorization on database::TAQBenchmarks to sa
 go
@@ -22,5 +24,5 @@ create master key encryption by password = 'P4ssw[]rd';
 
 /* In-memory only */
 alter database TAQBenchmarks add FILEGROUP InMemory contains memory_optimized_data;
-alter database TAQBenchmarks add file (name='inmemory_main', filename='b:\SQL\TAQ_inmemory') to filegroup InMemory;
+alter database TAQBenchmarks add file (name='inmemory_main', filename='D:\SQL\TAQ_inmemory') to filegroup InMemory;
 alter database TAQBenchmarks set memory_optimized_elevate_to_snapshot = on;
