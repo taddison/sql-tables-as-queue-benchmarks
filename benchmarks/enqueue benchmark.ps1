cc1cf69177685245cb0d08204616c9e54bbe55e6
Timothy Addison <timothy.addison@viagogo.com>
2016-05-22 16:08:03 +0100

    Add enqueue benchmark

---
+++ benchmarks/enqueue benchmark.ps1
@@ -0,0 +1,20 @@
+﻿# Enqueue tests - 1, 2, 4, 8 threads
+$connectionString = Get-Content .\connectionstring.txt
+$command = "exec dbo.EnqueueMessage @id=1;"
+$repeats = 5000
+
+$threads = 1
+write-host $threads "Threads executing " $command " " $repeats " times"
+.\SQLDriver.exe -c ""$connectionString"" -r $repeats -t $threads -s ""$command""
+
+$threads = 2
+write-host $threads "Threads executing " $command " " $repeats " times"
+.\SQLDriver.exe -c ""$connectionString"" -r $repeats -t $threads -s ""$command""
+
+$threads = 4
+write-host $threads "Threads executing " $command " " $repeats " times"
+.\SQLDriver.exe -c ""$connectionString"" -r $repeats -t $threads -s ""$command""
+
+$threads = 8
+write-host $threads "Threads executing " $command " " $repeats " times"
+.\SQLDriver.exe -c ""$connectionString"" -r $repeats -t $threads -s ""$command""
